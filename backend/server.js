646d1e80d456b8f360675bbe8b30dc006ea6c7c4
Mukuakuiza <hinomukuakuiza12@gmail.com>
2025-08-21 04:14:40 +0200

    setup the mongodb database

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,14 @@
 import express from "express";
+import dotenv from "dotenv";
+import  {connectDb} from "./config/db.js";
 
+dotenv.config();
 const app = express();
 
+app.get("/products",(req,res)=>{
+    res.send("server is ready")
+})
 app.listen(5000, ()=>{
+    connectDb();
     console.log("Server, started at http://localhost:5000")
 });
