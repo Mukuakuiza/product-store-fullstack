e3f3446fd21d15083c202196344e8e971c69328c
Mukuakuiza <hinomukuakuiza12@gmail.com>
2025-08-28 20:18:55 +0200

    final touch on the server setup

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -33,29 +33,34 @@ export const createProduct = async (req, res) => {
 
 export const updateProduct = async (req, res) => {
   const { id } = req.params;
+
   const product = req.body;
 
   if (!mongoose.Types.ObjectId.isValid(id)) {
     return res
       .status(404)
-      .json({ success: false, message: "Invalid product id" });
+      .json({ success: false, message: "Invalid Product Id" });
   }
 
   try {
-    const updateProduct = await Product.findByIdAndUpdate(id, product, {
+    const updatedProduct = await Product.findByIdAndUpdate(id, product, {
       new: true,
     });
-    res
-      .status(200)
-      .json({ success: true, message: "Product updated successfully" });
+    res.status(200).json({ success: true, data: updatedProduct });
   } catch (error) {
-    console.log("error in updating product:", error.message);
     res.status(500).json({ success: false, message: "Server Error" });
   }
 };
 
 export const deleteProduct = async (req, res) => {
   const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res
+      .status(404)
+      .json({ success: false, message: "Invalid Product Id" });
+  }
+
   try {
     await Product.findByIdAndDelete(id);
     res
@@ -63,6 +68,6 @@ export const deleteProduct = async (req, res) => {
       .json({ success: true, message: "Product deleted successfully" });
   } catch (error) {
     console.log("error in deleting product:", error.message);
-    res.status(404).json({ success: false, message: "Product not found." });
+    res.status(500).json({ success: false, message: "Server Error" });
   }
 };
--- backend/server.js
+++ backend/server.js
@@ -10,10 +10,12 @@ const app = express();
 //Middleware
 app.use(express.json());
 
+const PORT = process.env.PORT || 5000;
+
 //Routes
 app.use("/api/products", productRoutes);
 
-app.listen(5000, () => {
+app.listen(PORT, () => {
   connectDb();
-  console.log("Server, started at http://localhost:5000");
+  console.log("Server, started at http://localhost:" + PORT);
 });
